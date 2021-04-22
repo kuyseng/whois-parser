d961a38f7c39f3adff17c4d23afe24a11d0e6934
kuyseng <kuyseng@gmail.com>
2021-04-22 15:00:53 +0700

    fix: .xxx ParserError

--- lib/whois/parsers/whois.nic.xxx.rb
+++ lib/whois/parsers/whois.nic.xxx.rb
@@ -17,9 +17,27 @@ module Whois
     class WhoisNicXxx < BaseAfilias2
 
       self.scanner = Scanners::BaseAfilias, {
-          pattern_disclaimer: /^Access to/,
+          pattern_disclaimer: /^The WHOIS information|^The data in this record|^This service is|^Uniregistry reserves/,
           pattern_reserved: /^Reserved by ICM Registry\n/,
       }
+
+      property_supported :created_on do
+        node("Creation Date") do |value|
+          parse_time(value)
+        end
+      end
+
+      property_supported :updated_on do
+        node("Updated Date") do |value|
+          parse_time(value)
+        end
+      end
+
+      property_supported :expires_on do
+        node("Registry Expiry Date") do |value|
+          parse_time(value)
+        end
+      end
 
 
       property_supported :status do
