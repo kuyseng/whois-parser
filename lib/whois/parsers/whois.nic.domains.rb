6e4a8499e31f4196346d3ca25d7ca077588e3442
kuyseng <kuyseng@gmail.com>
2021-04-23 19:33:01 +0700

    added whois.nic.domains of Vibol
    Squashed commit of the following:
    
    commit e1d59b1795b83d061bce9b2bfc03a3623d883e1f
    Author: Vibol T <vibolteav@gmail.com>
    Date:   Fri Mar 5 09:53:29 2021 +0700
    
        fixed require
    
    commit daa3924d13723bd9252fb38264ce8cff347b4acf
    Author: Vibol T <vibolteav@gmail.com>
    Date:   Fri Mar 5 09:50:28 2021 +0700
    
        updated scanner
    
    commit 5c80cd24c201e581b2ed45bf622afc3c3c97b146
    Author: Vibol T <vibolteav@gmail.com>
    Date:   Fri Mar 5 09:50:12 2021 +0700
    
        Revert "fixed scanner"
    
        This reverts commit d97a0a38cdfda613ef36b89c98bde84e2b1de941.
    
    commit 3b16f1bcbadd9f3535fda9c807120948d5d0fac4
    Author: Vibol T <vibolteav@gmail.com>
    Date:   Fri Mar 5 09:42:43 2021 +0700
    
        fixed scanner
    
    commit c1952a3f3a8979cb2d8564ec911d3684ac120116
    Author: Vibol T <vibolteav@gmail.com>
    Date:   Thu Mar 4 17:05:33 2021 +0700
    
        fixed require
    
    commit 7a5054c14b994dec80ca631f1d891b19cae1bc88
    Author: Vibol T <vibolteav@gmail.com>
    Date:   Thu Mar 4 16:52:03 2021 +0700
    
        added whois.nic.domains

---
+++ lib/whois/parsers/whois.nic.domains.rb
@@ -0,0 +1,25 @@
+#--
+# Ruby Whois
+#
+# An intelligent pure Ruby WHOIS client and parser.
+#
+# Copyright (c) 2009-2018 Simone Carletti <weppos@weppos.net>
+#++
+
+
+require_relative 'base_verisign'
+require 'whois/scanners/base_icann_compliant'
+
+module Whois
+  class Parsers
+
+    # Parser for the whois.nic.domains server.
+    #
+    # @see Whois::Parsers::Example
+    #   The Example parser for the list of all available methods.
+    #
+    class WhoisNicDomains < BaseVerisign
+      self.scanner = Scanners::BaseIcannCompliant
+    end
+  end
+end
