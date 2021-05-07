85cc8b1746e2d5a656b58dd86b2b90b2d181ed47
kuyseng <kuyseng@gmail.com>
2021-05-07 20:44:26 +0700

    add parser whois.nic.agency

---
+++ lib/whois/parsers/whois.nic.agency.rb
@@ -0,0 +1,16 @@
+require_relative 'base_icb'
+
+
+module Whois
+  class Parsers
+
+    # Parser for the whois.nic.ac server.
+    #
+    # @see Whois::Parsers::Example
+    #   The Example parser for the list of all available methods.
+    #
+    class WhoisNicAgency < BaseIcb
+    end
+
+  end
+end
